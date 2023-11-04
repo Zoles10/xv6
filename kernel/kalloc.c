837d1be27b17b51f7b9b3bad8d0d81f7eb6783d1
Zoltan Raffay <villagerslovak@gmail.com>
2023-11-04 16:52:25 +0100

    add barrier

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -10,32 +10,37 @@
 #include "defs.h"
 
 void freerange(void *pa_start, void *pa_end);
+int pageindex(void *pa);
+void increase_ref(void *pa);
+int decrease_ref(void *pa);
 
 extern char end[]; // first address after kernel.
                    // defined by kernel.ld.
 
-struct run {
+struct run
+{
   struct run *next;
 };
 
-struct {
+struct
+{
   struct spinlock lock;
   struct run *freelist;
+  int refcount[(PHYSTOP - KERNBASE) / PGSIZE];
+  ;
 } kmem;
 
-void
-kinit()
+void kinit()
 {
   initlock(&kmem.lock, "kmem");
-  freerange(end, (void*)PHYSTOP);
+  freerange(end, (void *)PHYSTOP);
 }
 
-void
-freerange(void *pa_start, void *pa_end)
+void freerange(void *pa_start, void *pa_end)
 {
   char *p;
-  p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  p = (char *)PGROUNDUP((uint64)pa_start);
+  for (; p + PGSIZE <= (char *)pa_end; p += PGSIZE)
     kfree(p);
 }
 
@@ -43,18 +48,17 @@ freerange(void *pa_start, void *pa_end)
 // which normally should have been returned by a
 // call to kalloc().  (The exception is when
 // initializing the allocator; see kinit above.)
-void
-kfree(void *pa)
+void kfree(void *pa)
 {
   struct run *r;
 
-  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+  if (((uint64)pa % PGSIZE) != 0 || (char *)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
 
-  r = (struct run*)pa;
+  r = (struct run *)pa;
 
   acquire(&kmem.lock);
   r->next = kmem.freelist;
@@ -72,11 +76,48 @@ kalloc(void)
 
   acquire(&kmem.lock);
   r = kmem.freelist;
-  if(r)
+  if (r)
     kmem.freelist = r->next;
   release(&kmem.lock);
 
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
-  return (void*)r;
+  if (r)
+    memset((char *)r, 5, PGSIZE); // fill with junk
+  return (void *)r;
+}
+
+int pageindex(void *pa)
+{
+  return ((uint64)pa) / 4096;
+}
+
+void increase_ref(void *pa)
+{
+  acquire(&kmem.lock);
+  int idx = pageindex(pa);
+  if (idx < 0 || idx >= 1 << 15)
+  {
+    release(&kmem.lock);
+    return;
+  }
+  kmem.refcount[idx]++;
+  if (kmem.refcount[idx] == 1)
+  {
+    kmem.refcount[idx]++;
+  }
+  release(&kmem.lock);
+}
+
+int decrease_ref(void *pa)
+{
+  acquire(&kmem.lock);
+  int idx = pageindex(pa);
+  if (idx < 0 || idx >= 1 << 15)
+  {
+    release(&kmem.lock);
+    return 0;
+  }
+  --kmem.refcount[idx];
+  int count = kmem.refcount[idx];
+  release(&kmem.lock);
+  return count;
 }
